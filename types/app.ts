10b096cdb14d58e93432e4cd692caf9372aaba28
cwluvani <wh18710455626@126.com>
2022-03-28 21:29:09 +0800

    typescript intro

---
+++ types/app.ts
@@ -0,0 +1,19 @@
+class Student implements Person {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+let user = new Student("Chris", "A", "Wong");
+let welcome = greeter(user);
+
+console.log(welcome);
